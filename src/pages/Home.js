3e0e25f0aba8c69aaaa806103f51491d2167326e
khanshorif331 <khanshorif331@gmail.com>
2023-01-21 01:06:59 +0600

    data loaded successfully

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import ProductCard from '../components/ProductCard'
 
 const Home = () => {
-  return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
-    </div>
-  );
-};
+	const [products, setProducts] = useState([])
+	console.log(products)
 
-export default Home;
+	useEffect(() => {
+		fetch('https://moontech-server.vercel.app/products')
+			.then(res => res.json())
+			.then(data => setProducts(data.data))
+	}, [])
+
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+			{products.map(product => (
+				<ProductCard product={product}></ProductCard>
+			))}
+		</div>
+	)
+}
+
+export default Home
