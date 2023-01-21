714d6106f292fbec9f498a926687641d246c0b6f
khanshorif331 <khanshorif331@gmail.com>
2023-01-21 22:25:03 +0600

    productcard cart problem solve

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -7,7 +7,13 @@ const initialState = {
 const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
-	reducers: {},
+	reducers: {
+		addToCart: (state, action) => {
+			state.cart.push(action.payload)
+		},
+	},
 })
+
+export const { addToCart } = cartSlice.actions
 
 export default cartSlice.reducer
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React from 'react'
+import { useSelector } from 'react-redux'
+import ProductCard from '../components/ProductCard'
 
 const Cart = () => {
-  return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is cart</h1>
-    </div>
-  );
-};
+	const cart = useSelector(state => state.cart.cart)
+	return (
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+			{cart.map(product => (
+				<ProductCard product={product} />
+			))}
+		</div>
+	)
+}
 
-export default Cart;
+export default Cart
