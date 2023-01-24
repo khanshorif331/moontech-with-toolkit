9bfb09de2981c0d228f7b97c450a7c17201f6b45
khanshorif331 <khanshorif331@gmail.com>
2023-01-24 21:14:20 +0600

    middleware added

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartSlice from '../features/cart/cartSlice'
 import filterSlice from '../features/filter/filterSlice'
+import logger from 'redux-logger'
 
 const store = configureStore({
 	// devTools : false, // disable redux devtools
@@ -8,6 +9,7 @@ const store = configureStore({
 		cart: cartSlice,
 		filter: filterSlice,
 	},
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 })
 
 export default store
